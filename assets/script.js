c5bc7e48c80a5fa33be440a96cd820c2df5c7b6b
Hayden Beck <haydenabeck@gmail.com>
2021-04-08 17:50:33 -0500

    Added css styling and html classes. added for loops to change colors based off of time

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,16 @@ function displaytime(){
     var datetime=moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").text(datetime);
 }
+
 setInterval(displaytime,0);
+
+for (let i = 1; i < 10; i++) {
+    $('.timeOfDay' + i ).addClass('present')
+}
+for (let i = 1; i < 10; i++) {
+    $('.timeOfDay' + i ).addClass('past')
+}
+for (let i = 1; i < 10; i++) {
+    $('.timeOfDay' + i ).addClass('future')
+} 
+
